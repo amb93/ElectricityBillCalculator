5b1cf3721c94c942252238cce9d1df55cfb19e6c
amb93 <ahmad.elbaghdadi46@gmail.com>
2022-01-10 23:13:38 +0200

    updated comments on the code

--- Bill_Calculator.py
+++ Bill_Calculator.py
@@ -6,6 +6,13 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+# This code is to calculate the electricity bill in Jordan in case of two houses running on the same meter
+# The user inputs the data consumption of the second house with a meter that is not billed from the electricity company as follows:
+#   *the previous month's reading in KWh
+#   *the current month's reading in KWh
+#   *the total KWH consumption on the bill
+#   *the bill amount before taxes in Fils
+# the program will show how much the second house should pay with or without adding the taxes
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
